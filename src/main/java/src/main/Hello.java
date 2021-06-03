95b7e5d72b895a99772ae134d9adada072373074
liangxin <xiaofengguboy@163.com>
2021-06-03 15:15:15 +0800

    整合代码

--- src/main/java/src/main/Hello.java
+++ src/main/java/src/main/Hello.java
@@ -3,5 +3,9 @@ package src.main;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("你好世界");
+        System.out.println("hell world");
+        System.out.println("hell worl1");
+        System.out.println("程序代码A");
+        System.out.println("程序代码b");
     }
 }
